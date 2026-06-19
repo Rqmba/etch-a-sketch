893b64eab57f00084bb4670a3e4afdd0dae462e4
« Rqmba» <ramblack45@gmail.com>
2026-06-19 16:52:08 +0200

    feat: style grid with flexbox and fix square dimensions

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const currentDiv = document.querySelector("#container");
+const currentDiv = document.querySelector("#newDiv");
 // const gridButton = document.querySelector("#newGridButton");
 
 function createDiv() {
   for (let i = 0; i < 256; i++) {
     const square = document.createElement("div");
+    square.classList.add("square");
     currentDiv.appendChild(square);
   }
 }
