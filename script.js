2c292f42caddc7a2162b05a68f555421b3deb0a0
« Rqmba» <ramblack45@gmail.com>
2026-06-20 15:33:55 +0200

    feat: add new grid button with dynamic size and square scaling

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const currentDiv = document.querySelector("#newDiv");
-// const gridButton = document.querySelector("#newGridButton");
+const gridButton = document.querySelector("#newGridButton");
 
-function createDiv() {
-  for (let i = 0; i < 256; i++) {
+function createDiv(gridSize) {
+  for (let i = 0; i < gridSize * gridSize; i++) {
     const square = document.createElement("div");
     square.classList.add("square");
     currentDiv.appendChild(square);
+    let sizeSquare = 960 / gridSize;
+    square.style.height = sizeSquare + "px";
+    square.style.width = sizeSquare + "px";
     square.addEventListener("mouseover", () => {
       square.style.backgroundColor = "red";
       // J'ai mis red mais t'as compris l'idée
@@ -13,4 +16,14 @@ function createDiv() {
   }
 }
 
-createDiv();
+function chooseSize() {
+  let size = prompt("Choose your size");
+
+  if (size < 100 && size > 0) {
+    currentDiv.innerHTML = "";
+    createDiv(size);
+  }
+}
+
+gridButton.addEventListener("click", () => chooseSize());
+createDiv(16);
